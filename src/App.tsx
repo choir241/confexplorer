898f8e4d5b17a5944a674f5337a8fb83281e4952
choir241 <choir4558@gmail.com>
2025-10-06 14:30:37 -0400

    add: add conference data as array of objects with break down of categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
-export default function App(){
-  return(<></>)
+import { USAMap, type USAStateAbbreviation } from "@mirawision/usa-map-react";
+
+export default function App() {
+
+  const handleStateClick = (stateAbbreviation: USAStateAbbreviation) => {
+    console.log(`You clicked on ${stateAbbreviation}`);
+  };
+
+  const customStates = {
+    CA: {
+      onClick: handleStateClick,
+    },
+    TX: {
+      onClick: handleStateClick,
+    },
+  };
+  return <USAMap customStates={customStates} />;
 }
