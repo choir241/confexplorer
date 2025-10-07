840a27a494036ec51253b4f98f2f4184032a13f3
choir241 <choir4558@gmail.com>
2025-10-07 15:17:14 -0400

    add: add fills to respective states for each conference. grab conference data from respective clicked state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
 import { USAMap, type USAStateAbbreviation } from "@mirawision/usa-map-react";
+import { conferences } from "./static/conferenceData";
 
 export default function App() {
 
   const handleStateClick = (stateAbbreviation: USAStateAbbreviation) => {
     console.log(`You clicked on ${stateAbbreviation}`);
+    const selectedStateConf = conferences.filter(confData=>confData.state === stateAbbreviation)
+    alert(selectedStateConf[0].name);
   };
 
   const customStates = {
-    CA: {
+    WI: {
+      fill: "teal",
+      onClick: handleStateClick,
+    },
+    NY: {
+      fill: "#e12885",
+      onClick: handleStateClick,
+    },
+    NC: {
+      fill: "#b81c0b",
       onClick: handleStateClick,
     },
     TX: {
+      fill: "blue",
       onClick: handleStateClick,
     },
+    UT: {
+      fill: "purple",
+      onClick: handleStateClick
+    },
+    FL: {
+      fill: "lightblue",
+      onClick: handleStateClick
+    },
+    GA: {
+      fill: "orange",
+      onClick: handleStateClick
+    }
   };
-  return <USAMap customStates={customStates} />;
+  return <USAMap customStates={customStates}/>;
 }
